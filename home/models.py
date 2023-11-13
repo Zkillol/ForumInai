7d203c9ff0a1438e851a5f5926aedf3d6124027c
mtrza.maasaliev.01@gmail.com <mtrza.maasaliev.01@gmail.com>
2023-11-13 15:07:32 +0600

    added style

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Posts(models.Model):
     title = models.CharField( 'Название',max_length=50)
     description = models.TextField('Описание')
-    image = models.ImageField(upload_to='media/posts/' , null=True)
+    image = models.ImageField(upload_to='posts/' , null=True)
     # likes = models.PositiveIntegerField()
     # comments = models.CharField('Написать комментарий ' ,max_length=250 )
 
